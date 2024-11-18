6b9993e97112821194dcad88ac9dc5c7d71850ba
abduki <abdukiran@gmail.com>
2024-11-18 22:21:11 +0100

    add main layout with avatar, toolbar and badges

--- frontend/src/layouts/MainLayout.vue
+++ frontend/src/layouts/MainLayout.vue
@@ -1,109 +1,58 @@
 <template>
-  <q-layout view="lHh Lpr lFf">
-    <q-header elevated>
-      <q-toolbar>
-        <q-btn
-          flat
-          dense
-          round
-          icon="menu"
-          aria-label="Menu"
-          @click="leftDrawerOpen = !leftDrawerOpen"
-        />
-
-        <q-toolbar-title>
-          Quasar App
-        </q-toolbar-title>
-
-        <div>Quasar v{{ $q.version }}</div>
-      </q-toolbar>
+  <q-layout view="hHh lpR fFf">
+    <q-header class="bg-white text-dark q-mt-lg">
+      <div class="row justify-center no-wrap q-mb-md">
+        <div class="column justify-center items-center">
+          <q-avatar size="7rem" class="q-mb-lg">
+            <q-img
+              src='https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcQ_jvgHbdQJGnsp-pkTl2TqusMRD4lhW5yPUQ&s'
+            />
+          </q-avatar>
+          <q-toolbar-title>
+            Abdullah Kiran
+          </q-toolbar-title>
+        </div>
+      </div>
+      <div class="row justify-center q-mb-md badge-text-color">
+        <q-badge
+          outline
+          color="primary"
+          class="col-2 justify-center">
+          Projects
+        </q-badge>
+        <div class="col-1"/>
+        <q-badge
+          outline
+          color="primary"
+          class="col-2 justify-center">
+          Technologien
+        </q-badge>
+        <div class="col-1"/>
+        <q-badge
+          outline
+          color="primary"
+          class="col-2 justify-center">
+          Socials
+        </q-badge>
+      </div>
+      <q-separator color="gray" inset></q-separator>
+      <router-view></router-view>
     </q-header>
-
-    <q-drawer
-      v-model="leftDrawerOpen"
-      show-if-above
-      bordered
-      content-class="bg-grey-1"
-    >
-      <q-list>
-        <q-item-label
-          header
-          class="text-grey-8"
-        >
-          Essential Links
-        </q-item-label>
-        <EssentialLink
-          v-for="link in essentialLinks"
-          :key="link.title"
-          v-bind="link"
-        />
-      </q-list>
-    </q-drawer>
-
     <q-page-container>
-      <router-view />
+      <router-view/>
     </q-page-container>
+
   </q-layout>
 </template>
 
 <script>
-import EssentialLink from 'components/EssentialLink.vue'
-
-const linksData = [
-  {
-    title: 'Docs',
-    caption: 'quasar.dev',
-    icon: 'school',
-    link: 'https://quasar.dev'
-  },
-  {
-    title: 'Github',
-    caption: 'github.com/quasarframework',
-    icon: 'code',
-    link: 'https://github.com/quasarframework'
-  },
-  {
-    title: 'Discord Chat Channel',
-    caption: 'chat.quasar.dev',
-    icon: 'chat',
-    link: 'https://chat.quasar.dev'
-  },
-  {
-    title: 'Forum',
-    caption: 'forum.quasar.dev',
-    icon: 'record_voice_over',
-    link: 'https://forum.quasar.dev'
-  },
-  {
-    title: 'Twitter',
-    caption: '@quasarframework',
-    icon: 'rss_feed',
-    link: 'https://twitter.quasar.dev'
-  },
-  {
-    title: 'Facebook',
-    caption: '@QuasarFramework',
-    icon: 'public',
-    link: 'https://facebook.quasar.dev'
-  },
-  {
-    title: 'Quasar Awesome',
-    caption: 'Community Quasar projects',
-    icon: 'favorite',
-    link: 'https://awesome.quasar.dev'
-  }
-]
-
 export default {
-  name: 'MainLayout',
-  components: {
-    EssentialLink
-  },
-  data () {
-    return {
-      leftDrawerOpen: false,
-      essentialLinks: linksData
-    }
-  }
+  methods: {}
 }
 </script>
+
+<style scoped>
+.badge-text-color {
+  color: #000;
+}
+</style>
